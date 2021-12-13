ae047ce87f1e237148387d30e76dab8ddf6c9e2a
Mustafa Can Tekir <mustafacan.tekir@gmail.com>
2021-12-13 11:22:54 +0100

    adds ack and vimrc

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 brew tap homebrew/cask-versions
 brew install awscli aws-shell ansible coreutils colordiff go node typescript \
 	httpie htop git git-crypt groovy jmeter jq kafkacat maven python redis \
-	stunnel telnet tree watch openssl wget yarn peco zsh
+	stunnel telnet tree watch openssl wget yarn peco zsh ack
 brew install --cask iterm2 cryptomator rectangle spotify sublime-text google-chrome flux \
 	firefox-developer-edition slack docker skype bitwarden calibre evernote jetbrains-toolbox
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-for dotfile in .zshrc .gitconfig .gitignore
+for dotfile in .vim .zshrc .gitconfig .gitignore
 do
     ln -s -v -i ~/.dotfiles/${dotfile} ~
 done
